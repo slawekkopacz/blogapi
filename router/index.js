af62694b23c9cc77e1a61dfa27c556722ff44246
slawekkopacz <s.kopacz@gmail.com>
2016-05-25 18:34:50 +0200

    * first service added (postService)

--- router/index.js
+++ router/index.js
@@ -6,8 +6,10 @@ var router = express.Router();
 router.get('/', getRoot);
 router.use(postsRouter);
 
+
 function getRoot(req, res) {
   res.send('Hello World!');
 }
+
 
 module.exports = router;
--- router/posts/index.js
+++ router/posts/index.js
@@ -1,11 +1,30 @@
 ﻿var express = require('express');
+var postService = require('./../../services/postService.js');
 
 var router = express.Router();
 
-router.get('/posts', getPosts);
+router.get('/posts', getPostList);
+router.get('/posts/:id', getPost);
 
-function getPosts(req, res) {
-  res.send('Posts!');
+
+function getPostList(req, res) {
+  // TODO encapsulate in controller or sth?
+  postService.getAll(function (err, posts) {
+    if (err) {
+      handleError(err, res);
+    }
+
+    res.send(posts);
+  });
+}
+
+function getPost(req, res) {
+  res.send('Post id: ' + req.params.id);
+}
+
+function handleError(err, res) {
+  console.error(err);
+  res.status(500).send('Error 500 message');
 }
 
 module.exports = router;
